c3e3108779fb74a0be98c05c78175585080515a1
Vimal Khorajiya <vimal.c@masterlysolutions.com>
2025-10-06 15:28:29 +0530

    First Commit

---
+++ TeamCityPOC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TeamCityPOC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
